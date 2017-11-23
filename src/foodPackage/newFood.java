e870d992613601d9da0445918e7cbaa3031b5e1c
FlightTeam1 <prathusha.pandipati@gmail.com>
2017-11-23 16:57:26 +0100

    Updated FoodPackage

--- src/foodPackage/newFood.java
+++ src/foodPackage/newFood.java
@@ -1,14 +1,13 @@
 package foodPackage;
 
-import java.util.List;
-
 public interface newFood {
 	public void foodMenu();
 	public String foodName="";
 	public double itemPrice(int foodItem);
 	public double quantity();
-	public double calculatePrice(double quantity, double itemPrice);
+	
 	public void exit();
 	
-	List<newMenu> returnMenu();
+	
+	
 }
